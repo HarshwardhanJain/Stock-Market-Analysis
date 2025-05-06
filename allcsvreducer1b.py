8430993760f7213422bc90b863b21b969890e48e
HarshwardhanJain <harshwardhan11225@gmail.com>
2025-05-06 23:48:42 +0530

    Minor Formatting Changes

--- allcsvreducer1b.py
+++ allcsvreducer1b.py
@@ -10,4 +10,3 @@ df = pd.read_csv(sys.stdin,usecols=[1,2,3,4,5,6,7,8,9,10,11])
 df.columns = ['SYMBOL', 'SERIES', 'OPEN', 'HIGH', 'LOW', 'CLOSE', 'LAST', 'PREVCLOSE','TOTTRDQTY', 'TOTTRDVAL', 'TIMESTAMP']
 
 df[['SYMBOL', 'OPEN', 'HIGH', 'LOW','CLOSE']].to_csv(sys.stdout)
-
